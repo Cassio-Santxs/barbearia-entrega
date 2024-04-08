21741b77af8f4d3f753f88c5ae634fa3dd323866
Davi Mak <121460974+DaviMak@users.noreply.github.com>
2024-04-08 20:24:51 -0300

    o basico de funcionario feito já

--- src/main/java/app/entity/Funcionario.java
+++ src/main/java/app/entity/Funcionario.java
@@ -1,5 +1,60 @@
 package app.entity;
 
-public class Funcionario {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idFuncionario;
+	
+	private String nmFuncionario;
+	
+	private String tpFuncionario;
+	
+	private String dsCpf;
+	
+	private String dsLogin;
+	
+	private String dsSenha;
+	
+	
+	public long getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(long idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNmFuncionario() {
+		return nmFuncionario;
+	}
+	public void setNmFuncionario(String nmFuncionario) {
+		this.nmFuncionario = nmFuncionario;
+	}
+	public String getTpFuncionario() {
+		return tpFuncionario;
+	}
+	public void setTpFuncionario(String tpFuncionario) {
+		this.tpFuncionario = tpFuncionario;
+	}
+	public String getDsCpf() {
+		return dsCpf;
+	}
+	public void setDsCpf(String dsCpf) {
+		this.dsCpf = dsCpf;
+	}
+	public String getDsLogin() {
+		return dsLogin;
+	}
+	public void setDsLogin(String dsLogin) {
+		this.dsLogin = dsLogin;
+	}
+	public String getDsSenha() {
+		return dsSenha;
+	}
+	public void setDsSenha(String dsSenha) {
+		this.dsSenha = dsSenha;
+	}
 }
